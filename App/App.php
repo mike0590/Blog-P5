4a409f3355a1af63d567ac98db9f8d9b8f773594
Mike Gil <mike_gil@hotmail.fr>
2018-06-13 14:43:52 +0100

    Ajout de la class App (singleton) qui permettera d'instancier d'autres classes.

---
+++ App/App.php
@@ -0,0 +1,46 @@
+<?php
+
+
+namespace App;
+
+class App
+{
+
+	private static $db;
+	private static $instance;
+
+	public static function getDb()
+	{
+		if (self::$db == null) {
+			self::$db = new \App\DataBase();
+		}
+		return self::$db;
+	}
+
+	public static function getInstance()
+	{
+		if(self::$instance == null){
+			self::$instance = new App();
+		}
+		return self::$instance;
+
+	}
+
+	public function getTable($tableName)
+	{
+		$class_name = 'App\\Table\\' .ucfirst($tableName);
+		return new $class_name();
+	}
+
+
+	
+	public function forbidden()
+	{
+		header('HTTP/1.0 403 Forbidden');
+		die('Acces Interdiiiit');
+	}
+
+
+
+
+}
