3f10822047acfc6f7129a1c5030777a43224c915
Mike Gil <mike_gil@hotmail.fr>
2018-06-14 15:35:34 +0100

    Ajout de la fonction 'single' dans la class 'PostsController', qui va renvoyer la vue 'single.php'..

--- App/controller/PostsController.php
+++ App/controller/PostsController.php
@@ -36,4 +36,45 @@ class PostsController extends Controller
 		$this -> page('posts/posts', compact('posts', 'categories'));
 	}
 
+	public function single()
+	{
+		$this -> template = 'default_1';
+
+		$new = \App\App::getInstance() -> getTable('comments');
+		$visitor = new \App\Auth\DbAuth();
+		$id = $_GET['id'];
+
+
+		if (!empty($_POST) AND isset($_POST['pseudo']) AND isset($_POST['pass'])) {
+		    if($visitor -> userLogin($_POST['pseudo'], $_POST['pass']))
+		    {
+		      header('Location: index.php?p=single&id= '.$id.' ');
+		    }
+		    else
+		      ?>
+		    <div class=" lol alert alert-danger align" role="alert">Identifiants Incorrects</div>
+		    <?php }
+
+		elseif (!empty($_POST) AND isset($_POST['content'])) {
+		  $new -> addComment([
+		    'visitor_username' => $_SESSION['nameVisitor'],
+		    'content' => $_POST['content'],
+		    'posts_id' => $_GET['id'],
+		    'visitor_id' => $_SESSION['visitor'],
+		    'date' => 'NOW()'
+
+		  ]);
+		}
+
+		$comments = \App\App::getInstance() -> getTable('Comments');
+		$posts = \App\App::getInstance() -> getTable('posts');
+		$post = $posts -> getPost([$_GET['id']]);
+		$comment = $comments -> getComments([$_GET['id']]);
+
+		$this -> page('posts/single', compact('post', 'visitor', 'comments', 'comment'));
+
+	}
+
 }
+
+
