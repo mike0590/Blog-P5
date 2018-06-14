91c3af11ed02a6000ed41c11d56be64ed91c3023
Mike Gil <mike_gil@hotmail.fr>
2018-06-14 15:12:40 +0100

    Ajout de la vue (hors template) 'userLogin) qui affiche la page de connexion à l utilisateur.

---
+++ App/Views/posts/userLogin.php
@@ -0,0 +1,28 @@
+<?php
+
+
+
+ob_start();
+
+session_start();
+
+
+?>
+
+<form method="post" style="width: 30%; text-align: center; margin-left: auto; margin-right: auto;">
+
+<?php
+echo '</br>';
+echo $form -> input('username', 'Pseudo');
+echo $form -> input('password', 'Password', ['type' => 'password']). '</br>';
+echo $form -> submit('VALIDER');
+
+?>
+
+
+</form>
+
+<?php
+
+$userLogin = ob_get_clean();
+
