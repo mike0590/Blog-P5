f59b9dfea144cd709d4a50f467e20c882b0569df
Mike Gil <mike_gil@hotmail.fr>
2018-06-14 15:16:07 +0100

    Ajout du template 'default_2', qui servira pour toutes les pages administratives, ainsi que la page d authentification de l utilisateur..

---
+++ App/Views/template/default_2.php
@@ -0,0 +1,71 @@
+<!--
+author: W3layouts
+author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+	<title></title>
+	<!-- for-mobile-apps -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="keywords" content="Improve Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+	<script type="application/x-javascript">
+		addEventListener("load", function () {
+			setTimeout(hideURLbar, 0);
+		}, false);
+
+		function hideURLbar() {
+			window.scrollTo(0, 1);
+		}
+	</script>
+	<!-- //for-mobile-apps -->
+	<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+	<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+	<link href="css/font-awesome.css" rel="stylesheet">
+	<link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" />
+	<!-- css files -->
+	<link href="//fonts.googleapis.com/css?family=Josefin+Sans:100,100i,300,300i,400,400i,600,600i,700,700i&amp;subset=latin-ext" rel="stylesheet">
+	<link href="//fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+	<!-- //css files -->
+</head>
+
+<body>
+	<!-- banner -->
+	
+			
+				
+				<!-- //banner -->
+								
+	<!-- team -->
+	<!-- About us -->
+	<div class="about-3" style="width: 60%; margin-left: auto;margin-right: auto;">
+		<?php 
+		if ($p == 'userLogin'){
+			echo '<h3 class="w3l-titles">Login</h3>';
+			echo $userLogin;
+		}
+		
+		?>
+		<div class="container">
+			<div class="d-flex">
+			
+			</div>
+			
+		</div>
+	</div>
+	<!-- //About us -->
+
+	
+
+	<!-- Services -->
+		
+<!-- footer -->
+
+	<!-- js-scripts -->
+</body>
+
+</html>
