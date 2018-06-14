ab1fbc610121268a1ae3d0b5b5e4ce08585aa240
Mike Gil <mike_gil@hotmail.fr>
2018-06-14 16:57:43 +0100

    Ajout de la page 'admin.php' qui va appeller le controller 'AdminController' pour afficher toutes les pages administratives

---
+++ public/admin.php
@@ -0,0 +1,29 @@
+<?php
+
+define('ROOT', dirname(__DIR__));
+
+require ROOT. '/App/Autoloader.php';
+App\Autoloader::register();
+
+session_start();
+if (isset($_GET['p'])) {
+	$p = $_GET['p'];
+}
+else{
+	$p = 'admin';
+}
+
+
+$controller = new App\Controller\AdminController();
+$controller -> dashbord();
+
+
+
+
+
+
+
+if ($p == 'admin') {
+	$controller = new App\Controller\AdminController();
+	$controller -> index();
+}
