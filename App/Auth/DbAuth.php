27d7b82516228837d68f88df9ba1fc0620145b4d
Mike Gil <mike_gil@hotmail.fr>
2018-06-14 16:36:33 +0100

    Ajout des fonctions 'login' et 'logged' dans la class 'DbAuth'..

--- App/Auth/DbAuth.php
+++ App/Auth/DbAuth.php
@@ -32,6 +32,30 @@ class DbAuth
  		return false;
  }
 
+ public function login($username, $password)
+{
+	 $db = \App\App::getDb();
+	 $user = $db -> prepare('SELECT * FROM users WHERE username = ?', [$username], null, True);
+	 if ($user) {
+	 	if ($user -> password === sha1($password)){
+	 		session_start();
+	 		$_SESSION['auth'] = $user -> id;
+	 		return true;
+	    }
+	 }
+
+	 return false;
+ }
+
+ public function logged()
+ {
+ 	if (isset($_SESSION['auth'])){
+ 		return true;
+ 	}
+ 	else
+ 		return false;
+ }
+
 
 
 }
