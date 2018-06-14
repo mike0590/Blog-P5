a34f449361eb43afd8eda0d2f53ef1d752fe4d8d
Mike Gil <mike_gil@hotmail.fr>
2018-06-14 18:28:52 +0100

    Ajout du fichier singleComment.php, la vue qui montre un commentaire en attente en particulier.

---
+++ App/Views/admin/posts/singleComment.php
@@ -0,0 +1,21 @@
+<?php
+ob_start();
+?>
+
+<h3>Visiteur: <?= $comment -> visitor_username; ?></h3></br></br>
+<h3>Commentaire:</h3></br>
+	<?= $comment -> content; ?>
+</br></br></br>
+
+<tr>
+			
+			<td><a class="btn btn-primary" href="admin.php?p=commentAccepted&id=<?= $comment -> id; ?>">Accepter</a></td>
+			<td><a class="btn btn-danger" href="admin.php?p=commentDenied&id=<?= $comment -> id; ?>">Supprimer</a></td>
+			
+</tr>
+
+<a style="position: relative; bottom: 30px; left: 80%;" href="admin.php">Administration</a>
+
+
+<?php
+$singleComment = ob_get_clean();
